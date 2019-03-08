4534e9c148ed7b4a8f5891c3e13cc09bf9240d5b
cewdyn <daniel.brown@carbonblack.com>
2019-03-08 10:45:33 -0500

    Added script to package and send up lambda functions. Added test function and etl function.

---
+++ lambda/package_and_send_lambda.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+pip3 install -r $1/requirements.txt --target=$1/deps/
+cd $1/deps
+zip -r9 ../../package.zip *
+cd ..
+zip -r9 ../package.zip *.py
+cd ..
+
+aws lambda update-function-code --function-name $2 --zip-file fileb://package.zip --profile cewdyn
+
+rm package.zip
